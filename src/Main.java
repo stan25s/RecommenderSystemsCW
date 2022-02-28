7e4b0e8aba3659230609cfca2fdc5690f8fb01c5
Stanley Smith <stansmith2501@gmail.com>
2022-02-28 13:58:51 +0000

    Added CSV Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        // Taking csv file from command line args
+        String filename = args[0];
+        try {
+            Scanner sc = new Scanner(new File(filename));
+        } catch (Exception e) {
+            System.out.println("File Exception");
+        }
     }
 }
